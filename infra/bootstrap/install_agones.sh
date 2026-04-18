234063d9bd03a7854fc8f593244907703c457cf6
SirNoobCake <62817715+SirNoobCake@users.noreply.github.com>
2026-04-17 21:01:27 -0400

    undated helm install to upgrade --install

--- infra/bootstrap/install_agones.sh
+++ infra/bootstrap/install_agones.sh
@@ -3,4 +3,4 @@ set -e
 echo "Installing Agones via Helm..."
 helm repo add agones https://agones.dev/chart/stable
 helm repo update
-helm install my-release --namespace agones-system --create-namespace agones/agones
+helm upgrade my-release --install --namespace agones-system --create-namespace agones/agones
